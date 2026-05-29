eb7e425aef0d2ffd03fab648e07aef9c43c13ae2
Piotr <01189063@pw.edu.pl>
2026-05-29 17:30:05 +0200

    add csv table schema

---
+++ database_scripts/01_create_stg_education_costs.sql
@@ -0,0 +1,14 @@
+CREATE TABLE stg_education_costs (
+    Country VARCHAR(100),
+    City VARCHAR(100),
+    University VARCHAR(255),
+    Program VARCHAR(255),
+    Level VARCHAR(50),
+    Duration_Years FLOAT,
+    Tuition_USD FLOAT,
+    Living_Cost_Index FLOAT,
+    Rent_USD FLOAT,
+    Visa_Fee_USD FLOAT,
+    Insurance_USD FLOAT,
+    Exchange_Rate FLOAT
+);
