7bea89401e12938c222b2b030488986b07590120
Piotr <01189063@pw.edu.pl>
2026-06-09 20:47:39 +0200

    update company procedure to handle SCD2

--- database_scripts/usp_03_Load_DimCompanies_SCD2.sql
+++ database_scripts/usp_03_Load_DimCompanies_SCD2.sql
@@ -1,19 +1,38 @@
--- Procedura dla DimCompanies (tutaj zazwyczaj pojawiają się tylko nowe firmy)
 CREATE PROCEDURE usp_03_Load_DimCompanies_SCD2
 AS
 BEGIN
     SET NOCOUNT ON;
     DECLARE @CurrentDate DATETIME2 = GETDATE();
 
-    WITH SourceData AS (
-        SELECT DISTINCT company_name FROM stg_parsed_jobs WHERE company_name IS NOT NULL
-    )
-    
-    INSERT INTO DimCompanies (CompanyName, ValidFrom, ValidTo, IsCurrent)
-    SELECT s.company_name, @CurrentDate, NULL, 1
-    FROM SourceData s
+    UPDATE dim
+    SET 
+        dim.ValidTo = @CurrentDate,
+        dim.IsCurrent = 0
+    FROM DimCompanies dim
+    INNER JOIN (
+        SELECT DISTINCT company_name, industry 
+        FROM stg_parsed_jobs 
+        WHERE company_name IS NOT NULL
+    ) stg ON dim.CompanyName = stg.company_name
+    WHERE dim.IsCurrent = 1 
+      AND (dim.Industry <> stg.industry OR dim.Industry IS NULL AND stg.industry IS NOT NULL); 
+
+    INSERT INTO DimCompanies (CompanyName, Industry, ValidFrom, ValidTo, IsCurrent)
+    SELECT 
+        stg.company_name, 
+        stg.industry, 
+        @CurrentDate, 
+        NULL, 
+        1
+    FROM (
+        SELECT DISTINCT company_name, industry 
+        FROM stg_parsed_jobs 
+        WHERE company_name IS NOT NULL
+    ) stg
     WHERE NOT EXISTS (
-        SELECT 1 FROM DimCompanies t 
-        WHERE t.CompanyName = s.company_name AND t.IsCurrent = 1
+        SELECT 1 
+        FROM DimCompanies dim 
+        WHERE dim.CompanyName = stg.company_name 
+          AND dim.IsCurrent = 1
     );
 END;
