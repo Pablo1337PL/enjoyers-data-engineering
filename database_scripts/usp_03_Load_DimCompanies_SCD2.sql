9b96bd0cd6a77ff6a9584acef47728cdf5b69525
Piotr Wysocki <147279129+Wysocki-Piotr@users.noreply.github.com>
2026-06-09 22:08:22 +0200

    Refactor usp_03_Load_DimCompanies_SCD2 procedure
    Refactored the stored procedure to use a CTE for source data and removed the update logic.

--- database_scripts/usp_03_Load_DimCompanies_SCD2.sql
+++ database_scripts/usp_03_Load_DimCompanies_SCD2.sql
@@ -4,35 +4,22 @@ BEGIN
     SET NOCOUNT ON;
     DECLARE @CurrentDate DATETIME2 = GETDATE();
 
-    UPDATE dim
-    SET 
-        dim.ValidTo = @CurrentDate,
-        dim.IsCurrent = 0
-    FROM DimCompanies dim
-    INNER JOIN (
-        SELECT DISTINCT company_name, industry 
-        FROM stg_parsed_jobs 
-        WHERE company_name IS NOT NULL
-    ) stg ON dim.CompanyName = stg.company_name
-    WHERE dim.IsCurrent = 1 
-      AND (dim.Industry <> stg.industry OR dim.Industry IS NULL AND stg.industry IS NOT NULL); 
+    CREATE PROCEDURE usp_03_Load_DimCompanies_SCD2
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @CurrentDate DATETIME2 = GETDATE();
 
-    INSERT INTO DimCompanies (CompanyName, Industry, ValidFrom, ValidTo, IsCurrent)
-    SELECT 
-        stg.company_name, 
-        stg.industry, 
-        @CurrentDate, 
-        NULL, 
-        1
-    FROM (
-        SELECT DISTINCT company_name, industry 
-        FROM stg_parsed_jobs 
-        WHERE company_name IS NOT NULL
-    ) stg
+    WITH SourceData AS (
+        SELECT DISTINCT company_name FROM stg_parsed_jobs WHERE company_name IS NOT NULL
+    )
+    
+    INSERT INTO DimCompanies (CompanyName, ValidFrom, ValidTo, IsCurrent)
+    SELECT s.company_name, @CurrentDate, NULL, 1
+    FROM SourceData s
+
     WHERE NOT EXISTS (
-        SELECT 1 
-        FROM DimCompanies dim 
-        WHERE dim.CompanyName = stg.company_name 
-          AND dim.IsCurrent = 1
+        SELECT 1 FROM DimCompanies t 
+        WHERE t.CompanyName = s.company_name AND t.IsCurrent = 1
     );
 END;
